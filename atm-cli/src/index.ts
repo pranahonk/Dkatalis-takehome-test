d71cd2c9753d8463170badefd93dff88f40f0796
pranawijaya <pranajobs@hotmail.com>
2026-06-22 00:21:51 +0700

    feat: add cli entry point

--- atm-cli/src/index.ts
+++ atm-cli/src/index.ts
@@ -1,1 +1,33 @@
-export {};
+import readline from 'readline';
+import { Bank } from './Bank';
+import { parseCommand } from './CommandParser';
+import { executeCommand } from './CommandExecutor';
+
+const bank = new Bank();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function prompt(): void {
+  process.stdout.write('$ ');
+}
+
+prompt();
+
+rl.on('line', (line: string) => {
+  const parsed = parseCommand(line);
+  const outputLines = executeCommand(bank, parsed);
+
+  for (const outputLine of outputLines) {
+    console.log(outputLine);
+  }
+
+  console.log('');
+  prompt();
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});
